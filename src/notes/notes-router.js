ba6feb8f379c144067f02c1cc4acd799d3323751
Trent Smith <trentnsmith@gmail.com>
2020-05-10 20:57:41 -0500

    typo fix

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -4,12 +4,19 @@ const NotesService = require('./notes-service');
 const notesRouter = express.Router();
 const jsonParser = express.json();
 
+const serializeNote = note => ({
+    id: note.id,
+    note_name: note.note_name,
+    content: note.content,
+    folder_id: note.folder_id
+})
+
 notesRouter
     .route('/')
     .get((req, res, next) => {
         NotesService.getAllNotes(req.app.get('db'))
             .then(note => {
-                res.json(note)
+                res.json(note.map(serializeNote))
             })
             .catch(next)
     })
@@ -33,7 +40,7 @@ notesRouter
             res
                 .status(201)
                 .location(path.posix.join(req.originalUrl, `/${note.id}`))
-                .json(note)
+                .json(serializeNote(note))
         })
         .catch(next)
     })
@@ -56,18 +63,40 @@ notesRouter
             next()
         })
     })
-    .get((res, res, next) => {
-        res.json(res.note)
+    .get((req, res, next) => {
+        res.json(serializeNote(res.note))
     })
-    .delete((res, res, next) => {
+    .delete((req, res, next) => {
         NotesService.deleteNote(
             req.app.get('db'),
             req.params.note_id
         )
-        .then(() => {
+        .then(numRowsAffected => {
             res.status(204).end()
         })
         .catch(next)
     })
+    .patch(jsonParser, (req, res, next) => {
+        const { text, date } = req.body;
+        const noteToUpdate = { text, date };
+    
+        const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length
+        if (numberOfValues === 0)
+          return res.status(400).json({
+            error: {
+              message: `Request body must contain either 'text' or 'date'`
+            }
+          });
+    
+        NotesService.updateNote(
+          req.app.get('db'),
+          req.params.note_id,
+          noteToUpdate
+        )
+          .then(numRowsAffected => {
+            res.status(204).end();
+          })
+          .catch(next);
+      });
     
     module.exports = notesRouter
