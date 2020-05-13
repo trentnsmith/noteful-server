f8e24ccb528b53a1558046f01e2cbe48439ea7d5
Trent Smith <trentnsmith@gmail.com>
2020-05-12 22:51:57 -0500

    ..

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-    "migrationsDirectory": "migrations",
+    "migrationDirectory": "migrations",
     "driver": "pg",
-    "connectionString": process.env.DATABASE_URL,
+    "connectionString": (process.env.NODE_ENV === 'test')
+        ? process.env.TEST_DATABASE_URL
+        : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
 }
